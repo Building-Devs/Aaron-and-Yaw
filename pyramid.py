f0ef23e139a49e722e633c5dd20e4bb97c7f8ee9
Red <yawoffeh123@gmail.com>
2022-07-15 05:47:07 +0100

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -8,8 +8,18 @@ def pyramid_one_side(num):
     return "".join(res)
 
 def pyramid_space_between(num):
-    pass
-
+    res = []
+    cnt = num - 1
+    bottom = num+(num-1)
+    for i in range(1, (bottom) + 1, 2):
+        if i==1:
+            res.append(" "*num + "*" +"\n")
+            cnt -= 1
+            continue
+        pattern = " " * (cnt) + "*" + " " * i + "*" + "\n"
+        res.append(pattern)
+        cnt -= 1
+    return "".join(res)
 #in ascending order
 def pyramid_nums_between(num):
     pass
@@ -18,18 +28,38 @@ def pyramid_nums(num):
     pass
 
 def pyramid(num):
-    pass
+    res = []
+    cnt = num - 1
+    bottom = num+(num-1)
+    for i in range(1, (bottom) + 1, 2):
+        pattern = " " * cnt + "*" * i + "\n"
+        res.append(pattern)
+        cnt -= 1
+    return "".join(res)
 
+def main():
+    try:
+        t = int(input())
+        print(pyramid_one_side(t))
+        print(pyramid_space_between(t))
+        print(pyramid(t))
+    except ValueError:
+        print("Invalid input!!")
+        main()
+
+if __name__ == "__main__":
+    os.system("clear")
+    main()
 
 """
 if user input is for example 5
 output>>
 1.
-    *
-    **
-   ***
-  ****
- *****
+     *
+    ***
+   *****
+  *******
+ *********
 2.
 *
 **
@@ -61,5 +91,12 @@ output>>
  *   3   *
 *    4    *
 
+process
+
+difference in space [num-1]---> *
+                               ***
+                              *****
+                             *******
+bottom [num+= num-1]--->    *********
 
 """
